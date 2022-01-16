8266100705657fccc9fb1dd3590adbb12d9df6f9
DKorir <dominic.korir@student.moringaschool.com>
2022-01-16 16:20:26 +0300

    initial commit create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './github-app/github-app.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubProfileDataComponent } from './github-profile-data/github-profile-data.component';
+import { GithubProfileReposComponent } from './github-profile-repos/github-profile-repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubProfileDataComponent,
+    GithubProfileReposComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/github-profile-repos/github-profile-repos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-profile-repos',
+  templateUrl: './github-profile-repos.component.html',
+  styleUrls: ['./github-profile-repos.component.css']
+})
+export class GithubProfileReposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
