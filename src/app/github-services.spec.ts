9ecee0a286a7a1dc90cae60b6b7721b035613139
DKorir <dominic.korir@student.moringaschool.com>
2022-01-16 16:24:16 +0300

    initial commit create github service class

---
+++ src/app/github-services.spec.ts
@@ -0,0 +1,7 @@
+import { GithubServices } from './github-services';
+
+describe('GithubServices', () => {
+  it('should create an instance', () => {
+    expect(new GithubServices()).toBeTruthy();
+  });
+});
